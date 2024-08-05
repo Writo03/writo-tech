975781a5cd3ba5fbaf83fd590d22564c7b64c498
Writo03 <88578386+Writo03@users.noreply.github.com>
2024-08-05 23:59:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 
 // const app = express()
 app.use(cors({
-    origin: ["http://localhost:3000", 'https://writo-education-frontend.vercel.app/'],
+    origin: ["http://localhost:3000", 'https://writo-tech.vercel.app/'],
     credentials: true
 }))
 app.use(express.json())
